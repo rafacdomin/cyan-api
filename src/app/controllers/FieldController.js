90ee3d0d9feb80d16bbb18d2eb91aaf407bfe3b4
Rafael <rafaelcodomingues@gmail.com>
2020-09-17 14:29:12 -0300

    :hammer_and_wrench: Create field modifications

--- src/app/controllers/FieldController.js
+++ src/app/controllers/FieldController.js
@@ -1,30 +1,118 @@
+import { validate } from 'uuid';
 import AppError from '../../errors/AppError';
 import Farm from '../models/Farm';
 import Field from '../models/Field';
+import Harvest from '../models/Harvest';
+import Mill from '../models/Mill';
 
 export default {
   async store(req, res) {
-    const { latitude, longitude, farm_id } = req.body;
+    const {
+      latitude,
+      longitude,
+      farm,
+      harvest,
+      start_date,
+      end_date,
+      mill,
+    } = req.body;
+    const { user_id } = req;
 
-    const farm = await Farm.findByPk(farm_id, {
-      include: { association: 'harvest', include: { association: 'mill' } },
+    // Mill validation
+
+    let newMill = await Mill.findOne({ where: { name: mill, user_id } });
+
+    if (validate(mill)) {
+      newMill = await Mill.findByPk(mill);
+
+      if (newMill.user_id !== user_id) {
+        throw new AppError('User unathourized', 401);
+      }
+    }
+
+    if (!newMill) {
+      try {
+        newMill = await Mill.create({ name: mill, user_id });
+      } catch (err) {
+        throw new AppError('Name must be unique');
+      }
+    }
+
+    // Harvest Validation
+
+    let newHarvest = Harvest.build({
+      start_date,
+      end_date,
+      mill_id: newMill.id,
     });
 
-    if (!farm) {
-      throw new AppError("Farm doesn't exist");
+    if (harvest) {
+      newHarvest = await Harvest.findByPk(harvest, {
+        include: { association: 'mill' },
+      });
+
+      if (newHarvest.mill.user_id !== user_id) {
+        throw new AppError('User unathourized', 401);
+      }
+
+      if (!newHarvest) {
+        newHarvest = await Harvest.create({
+          start_date,
+          end_date,
+          mill_id: newMill.id,
+        });
+      }
+    } else {
+      await newHarvest.save();
     }
 
-    if (req.user_id !== farm.harvest.mill.user_id) {
-      throw new AppError("User doesn't have permission", 401);
+    // Farm Validation
+
+    let newFarm = await Farm.findOne({
+      where: { name: farm },
+      include: {
+        association: 'harvest',
+        include: { association: 'mill' },
+      },
+    });
+
+    if (newFarm.harvest.mill.user_id !== user_id) {
+      newFarm = null;
     }
 
-    const field = await Field.create({
+    if (validate(farm)) {
+      newFarm = await Farm.findByPk(farm, {
+        include: {
+          association: 'harvest',
+          include: { association: 'mill' },
+        },
+      });
+
+      if (newFarm.harvest.mill.user_id !== user_id) {
+        throw new AppError('User unathourized', 401);
+      }
+    }
+
+    if (!newFarm) {
+      newFarm = await Farm.create({ name: farm, harvest_id: newHarvest.id });
+    }
+
+    // Field Validation
+
+    const newField = Field.build({
       geography: { type: 'Point', coordinates: [latitude, longitude] },
-      farm_id,
+      farm_id: newFarm.id,
     });
 
-    return res.json(field);
+    try {
+      await newField.save();
+    } catch (err) {
+      throw new AppError('Latitude and Longitude must be unique');
+    }
+
+    return res.json(newField);
   },
+
   async index(req, res) {
     const fields = await Field.findAll({
       include: {
